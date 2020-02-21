daaef6056657f436d84af47b8652f509ad20c57f
Tisdale Fry <tisdalefry@gmail.com>
2020-02-21 22:33:51 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,14 +1,14 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = ""
   puts phrase
 end
 
-
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 def toadstool
   puts status
 end
 
 def link 
-  phrase = "It's Dangerous To Go Alone! Take This."
-  return phrase
+  line2 = "It's Dangerous To Go Alone! Take This."
+  return line2
 end
